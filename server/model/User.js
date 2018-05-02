fa6beb563e4a6e4d92cbb70ceb7877ad41456910
Dilip Bhattarai <synister.dilip@gmail.com>
2018-05-02 22:14:57 +0545

    Mongoose and Postman / serverjs / moving codes into the db and mongoose folder

---
+++ server/model/User.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+	name: {
+		type: String,
+		required : true,
+		minlength: 1,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	age: {
+		type: Number,
+		default: null
+	},
+	location: {
+		type: String,
+		default: 'N/A'
+	},
+	completedAt: {
+		type: Number,
+		default: null 
+	}
+});
+
+module.exports = {
+	User 
+};
+
+// // save new something
+// var NewUser = new User({
+// 	name: 'Dilip Bhattarai',
+// 	email: 'synister.dilip@gmail.com',
+// 	age: 22,
+// 	location: 'Madhumalla, Morang',
+// 	completedAt: 1234156
+// });
+
+// NewUser.save().then((doc) => {
+// 	console.log('Saved User', doc);
+// }, (e) => {
+// 	console.log('Unable to save todo', e);
+// });
+
+
+
+
+// var NewUser = new User({
+// 	name: 'Dilip Bhattarai',
+// 	age: 22,
+// 	location: 'Madhumalla, Morang',
+// 	completedAt: 1234156
+// });
+
+// NewUser.save().then((doc) => {
+// 	console.log('Saved User', doc);
+// }, (e) => {
+// 	console.log('Unable to save todo');
+// });
---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./model/User');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/users', (request, response) => {
+	var newUser = new User({
+		name: request.body.name,
+		email: request.body.email
+	});
+
+	newUser.save().then((doc) => {
+		response.send(doc);
+	}, (e) => {
+		response.status(400).send(e);
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Started on port 3000');
+});
+
+
+
+
+

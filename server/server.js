620c4ac94c3241a91f13c76273517bced1ce2a82
Dilip Bhattarai <synister.dilip@gmail.com>
2018-05-03 10:52:00 +0545

    Adding GET /user:id route and validating it

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./model/User');
@@ -7,6 +8,7 @@ var {User} = require('./model/User');
 var app = express();
 app.use(bodyParser.json());
 
+//POST a user
 app.post('/users', (request, response) => {
 	var newUser = new User({
 		name: request.body.name,
@@ -20,6 +22,7 @@ app.post('/users', (request, response) => {
 	});
 });
 
+//GET all users
 app.get('/users', (request, response) => {
 	User.find().then((users) => {
 		response.send({users});
@@ -27,6 +30,32 @@ app.get('/users', (request, response) => {
 		response.send(400).send(e);
 	});
 })
+
+//GET users/:id
+app.get('/users/:id', (request, response) => {
+	var id = request.params.id;
+
+	//Validating id using mongoose query isValid()
+	if(!ObjectID.isValid(id)){
+		return response.status(404).send();
+	}
+
+	User.findById(id).then((user) => {
+		if(!user){
+			response.status(404).send(user);
+		}
+		response.status(200).send(user);
+	}, (err) => {
+		response.send(400).send(err);
+	});
+
+	// User.isValid(id).then((err) => {
+	// 	if(err){
+	// 		return response.status(404).send('User not found');
+	// 	}
+
+	// }).catch(e) => response.status(400).send(e);
+});
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
